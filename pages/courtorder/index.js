b5389b8a4ca6a06e17eb816ea73973df154dedb6
Arshad Kaleem <47106714+arshadkaleem@users.noreply.github.com>
2025-03-17 01:16:00 +0530

    court order Update index.js

--- pages/courtorder/index.js
+++ pages/courtorder/index.js
@@ -1,54 +1,56 @@
-import React from "react";
-import { FaFilePdf } from "react-icons/fa";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import * as AllApi from "../../components/allApi/TitleDocApi";
 import Navigation from "../../components/home/Navigation";
 import SubPageHeader from "../../components/SubPageHeader";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import HeadMeta from "../../components/HeadMeta";
 
-const Index = ({ citizenData }) => {
+const Index = () => {
   const { t } = useTranslation("common");
   const router = useRouter();
-
   return (
     <div>
-       <HeadMeta
+      <HeadMeta
         title="The Directorate of Archaeology and Museums functions under the Department of Culture Affairs, Government of Maharashtra."
         content="The Directorate of Archaeology and Museums functions under the Department of Culture Affairs, Government of Maharashtra and is further divided into two branches- Archaeology and Museums. The Directorate operates through its sub-offices at Ratnagiri, Nashik, Pune, Aurangabad, Nanded and Nagpur for administrative purposes."
       />
       <Navigation pagePath={router.route} />
-      <SubPageHeader title={t("CitizenCharter")} />
-      <div className="w-full px-2 py-10 sm:px-0 sm:w-11/12 mx-auto">
-        <p className="text-justify text-lg pb-10 text-slate-700">
-          {t("CitizenCharter-p")}
-        </p>
-        <p className="text-left  py-2 font-semibold">{t("Download")}</p>
-        <div className="flex   ">
-          <ul className="list-none">
-            {citizenData != undefined &&
-              citizenData.map((item) => (
-                <li
-                  key={Math.random() * 100}
-                  className="pb-2 text-base text-slate-700 hover:font-semibold"
-                >
-                  <a
-                    href={item.file.url}
-                    target="_blank"
-                    key={Math.random() * 100}
-                    rel="noopener noreferrer"
-                    className=" pb-3 "
-                  >
-                    <span className="inline-block text-slate-700">
-                      <FaFilePdf />
-                    </span>{" "}
-                    {router.locale === "en" ? item.titleInEnglish : item.title}
+      <SubPageHeader title="Court Order" />
+      <div className="mt-5">
+        <section className="">
+          <div className="px-5 py-3 mx-auto">
+            <div className=" w-full mx-auto overflow-auto container ">
+              <ul className="w-full mx-auto">
+                <li className="text-lg text-justify text-slate-700">
+                  <a href="https://assets.ctfassets.net/l47hkrce4wip/5cMtASSKSPIZnQvK43L6yX/47e260fe422a91f3f06b65d94bab9978/2699-2023_Order_Vishalgad_Fort__11.02.2025.pdf">
+                    2699-2023
                   </a>
                 </li>
-              ))}
-          </ul>
-        </div>
+
+<li className="text-lg text-justify text-slate-700">
+                  <a href="https://assets.ctfassets.net/l47hkrce4wip/6P3JTPFKbgjltZAtOjiGK4/a17da9bd5465bfe38b961859c36e8855/1972-2023_Order_Vishalgad_Fort_11.02.2025.pdf">
+                    1972-2023
+                  </a>
+                </li>
+<li className="text-lg text-justify text-slate-700">
+                  <a href="https://assets.ctfassets.net/l47hkrce4wip/5NVPsaPGEsCDQZU7kMnpSr/004fc2d010768b6e6e0c35f3445b1494/9968-2024_Order_Vishalgad_Fort_11.02.2025.pdf">
+                    9968-2024
+                  </a>
+                </li>
+
+<li className="text-lg text-justify text-slate-700">
+                  <a href="https://assets.ctfassets.net/l47hkrce4wip/GZqWUvhPB5rHht60nZZ73/7a1c09fe53a96b5a72b065891d8c236c/10774-2024_Order_Vishalgad_Fort_11.02.2025.pdf">
+                    10774-2024
+                  </a>
+                </li>
+
+
+
+  
+              </ul>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
@@ -56,14 +58,8 @@ const Index = ({ citizenData }) => {
 
 export default Index;
 
-export async function getServerSideProps({ locale }) {
-  const { data } = await AllApi.titleDocAPI("Citizen Charter");
-  const citizenData = data.actsAndLegislationCollection.items;
-
-  return {
-    props: {
-      citizenData,
-      ...(await serverSideTranslations(locale, ["common"])),
-    },
-  };
-}
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["common"])),
+  },
+});
